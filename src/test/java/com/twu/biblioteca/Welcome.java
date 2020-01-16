4c1f34a172caafabf8adb6ebfd8fe6d3d6d90fde
Stephano Ramos <stephano.ramos@thoughtworks.com>
2020-01-16 09:22:12 -0300

    Refactored to use a static final constant to show de message

--- src/test/java/com/twu/biblioteca/Welcome.java
+++ src/test/java/com/twu/biblioteca/Welcome.java
@@ -4,12 +4,13 @@ import java.io.PrintStream;
 
 public class Welcome {
     private PrintStream printStream;
+    private static final String MESSAGE = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
 
     public Welcome(PrintStream printStream) {
         this.printStream = printStream;
     }
 
     public void show() {
-        printStream.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        printStream.println(MESSAGE);
     }
 }
