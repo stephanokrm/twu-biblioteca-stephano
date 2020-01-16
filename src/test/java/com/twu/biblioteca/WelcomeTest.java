10ccbc580a47fc05ef423444856bdb2904e8eae7
Stephano Ramos <stephano.ramos@thoughtworks.com>
2020-01-16 09:27:07 -0300

    Renamed test

--- src/test/java/com/twu/biblioteca/WelcomeTest.java
+++ src/test/java/com/twu/biblioteca/WelcomeTest.java
@@ -19,7 +19,7 @@ public class WelcomeTest {
     }
 
     @Test
-    public void shouldWelcomeMessageWhenIStartTheApplication() {
+    public void shouldWelcomeMessage() {
         welcome.show();
 
         verify(printStream).println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
