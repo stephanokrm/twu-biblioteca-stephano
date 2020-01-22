95cfea3dee9393e17c96f61388af765b6bfca14f
Stephano Ramos <stephano.ramos@thoughtworks.com>
2020-01-22 10:14:35 -0300

    Moved the class initialization to inside the try block

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -8,10 +8,11 @@ import java.io.PrintStream;
 public class BibliotecaApp {
     public static void main(String[] args) {
         PrintStream out = System.out;
-        Question question = new Question(System.out, System.in);
-        Application application = new Application(question, out);
 
         try {
+            Question question = new Question(System.out, System.in);
+            Application application = new Application(question, out);
+
             application.boot();
             application.run();
         } catch (Exception exception) {
