5eaa563a3f8ae382525365eed2ddc7f4499cd39e
Stephano Ramos <stephano.ramos@thoughtworks.com>
2020-01-23 16:44:46 -0300

    Added movies to the repository

--- src/main/java/com/twu/biblioteca/repository/MovieRepository.java
+++ src/main/java/com/twu/biblioteca/repository/MovieRepository.java
@@ -2,10 +2,18 @@ package com.twu.biblioteca.repository;
 
 import com.twu.biblioteca.model.Movie;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MovieRepository implements Repository<Movie> {
+    private static final List<Movie> movies = new ArrayList<>(Arrays.asList(
+            new Movie("The Irishman", 2019, "Martin Scorsese", 10, true),
+            new Movie("Parasite", 2019, "Bong Joon-ho", 9, true),
+            new Movie("Marriage Story", 2019, "Noah Baumbach", 8, true)
+    ));
+
     public List<Movie> all() {
-        return null;
+        return movies;
     }
 }
