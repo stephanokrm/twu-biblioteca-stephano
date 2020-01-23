329436d6a68b7681f395d74e66cd570efaa33ed9
Stephano Ramos <stephano.ramos@thoughtworks.com>
2020-01-23 11:53:33 -0300

    Returning a book

--- src/main/java/com/twu/biblioteca/domain/menu/option/ReturnABookOption.java
+++ src/main/java/com/twu/biblioteca/domain/menu/option/ReturnABookOption.java
@@ -1,6 +1,8 @@
 package com.twu.biblioteca.domain.menu.option;
 
+import com.twu.biblioteca.exception.BookNotAvailableException;
 import com.twu.biblioteca.foundation.Question;
+import com.twu.biblioteca.model.Book;
 import com.twu.biblioteca.service.BookService;
 
 import java.io.PrintStream;
@@ -9,15 +11,19 @@ public class ReturnABookOption extends MenuOption {
     public static final int NUMBER = 3;
 
     private Question question;
+    private BookService bookService;
 
     public ReturnABookOption(PrintStream out, Question question, BookService bookService) {
         super(NUMBER, "Return a Book");
 
         this.question = question;
+        this.bookService = bookService;
     }
 
     @Override
-    public void show() {
-        question.askForString("Enter the book title: ");
+    public void show() throws BookNotAvailableException {
+        String title = question.askForString("Enter the book title: ");
+        Book book = bookService.getBookByTitle(title);
+        bookService.returnBook(book);
     }
 }
