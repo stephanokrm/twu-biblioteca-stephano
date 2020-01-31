a6a34e14bd2e4cdb69864d224c8a520bbba8f8c9
Stephano Ramos <stephano.ramos@thoughtworks.com>
2020-01-31 14:04:19 -0300

    Adding the option to checkout a movie

--- src/main/java/com/twu/biblioteca/domain/menu/option/CheckoutAMovieOption.java
+++ src/main/java/com/twu/biblioteca/domain/menu/option/CheckoutAMovieOption.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca.domain.menu.option;
 
 import com.twu.biblioteca.foundation.Question;
+import com.twu.biblioteca.model.Movie;
 import com.twu.biblioteca.service.MovieService;
 
 import java.io.PrintStream;
@@ -8,14 +9,18 @@ import java.io.PrintStream;
 public class CheckoutAMovieOption extends MenuOption {
     public static final int NUMBER = 5;
     private final Question question;
+    private final MovieService movieService;
 
     public CheckoutAMovieOption(PrintStream out, Question question, MovieService movieService) {
         super(NUMBER, "Checkout a Movie");
         this.question = question;
+        this.movieService = movieService;
     }
 
     @Override
-    public void show() throws Exception {
-        question.askForString("Enter the movie name: ");
+    public void show() {
+        String name = question.askForString("Enter the movie name: ");
+        Movie movie = movieService.getMovieByName(name);
+        movieService.checkOutMovie(movie);
     }
 }
