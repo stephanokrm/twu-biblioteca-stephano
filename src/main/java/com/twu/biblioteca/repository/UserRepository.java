259c1bf8ce75d7c751b26de8e2295fdc56532eda
Stephano Ramos <stephano.ramos@thoughtworks.com>
2020-02-04 12:49:58 -0300

    Added default user

--- src/main/java/com/twu/biblioteca/repository/UserRepository.java
+++ src/main/java/com/twu/biblioteca/repository/UserRepository.java
@@ -3,11 +3,16 @@ package com.twu.biblioteca.repository;
 import com.twu.biblioteca.model.User;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class UserRepository implements Repository<User> {
+    private static final List<User> users = new ArrayList<>(Arrays.asList(
+            new User("xxx-xxxx", "Password")
+    ));
+
     @Override
     public List<User> all() {
-        return new ArrayList<>();
+        return users;
     }
 }
